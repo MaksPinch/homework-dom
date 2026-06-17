25c68fb5f97e836a1ed70e7b0ebd60a4346d6e2b
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2026-06-17 16:07:52 +0200

    event handling is done

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,130 @@
-const MOVE_INTERVAL_MS = 1000;
-document.addEventListener("DOMContentLoaded", () => {
-  const holes = document.querySelectorAll(".hole");
+class Gameboard {
+  constructor() {
+    this.holes = document.querySelectorAll(".hole");
 
-  if (holes.length == 0) {
-    throw new Error("Не найдены элементы .hole на странице");
+    this.winsEl = document.getElementById("score-wins");
+
+    this.lossesEl = document.getElementById("score-losses");
+
+    this.boardEl = document.querySelector(".board");
   }
 
-  const boardSize = holes.length;
-  const goblin = document.createElement("img");
-
-  if (typeof process !== "undefined" && process.env.NODE_ENV === "test") {
-    goblin.src = "#";
-  } else {
-    goblin.src = require("../img/goblin.png");
+  updateCounters(wins, losses) {
+    if (this.winsEl) this.winsEl.textContent = wins;
+    if (this.lossesEl) this.lossesEl.textContent = losses;
   }
+}
 
-  goblin.classList.add("goblin-img");
-  let currentHoleIndex = -1;
+class Goblin {
+  constructor() {
+    this.element = document.createElement("img");
 
-  let moveIntervalId = null;
+    this.element.classList.add("goblin-img");
 
-  function moveGoblin() {
-    let newIndex;
-
-    do {
-      newIndex = Math.floor(Math.random() * boardSize);
-    } while (newIndex === currentHoleIndex);
-
-    holes[newIndex].append(goblin);
-    currentHoleIndex = newIndex;
-  }
-
-  function stopGame() {
-    if (moveIntervalId !== null) {
-      clearInterval(moveIntervalId);
-      moveIntervalId = null;
+    if (typeof process !== "undefined" && process.env.NODE_ENV === "test") {
+      this.element.src = "#";
+    } else {
+      this.element.src = require("../img/goblin.png");
     }
   }
 
-  moveGoblin();
+  remove() {
+    if (this.element.parentNode) {
+      this.element.parentNode.removeChild(this.element);
+    }
+  }
+}
 
-  moveIntervalId = setInterval(moveGoblin, MOVE_INTERVAL_MS);
+const MOVE_INTERVAL_MS = 1000;
+const MAX_LOSSES = 5;
+
+class GameController {
+  constructor(board, goblin) {
+    this.board = board;
+    this.goblin = goblin;
+
+    this.wins = 0;
+    this.losses = 0;
+    this.currentHoleIndex = -1;
+    this.moveIntervalId = null;
+    this.goblinClickedInThisTurn = false;
+  }
+
+  init() {
+    if (this.board.holes.length === 0) return;
+
+    if (this.board.boardEl) {
+      this.board.boardEl.addEventListener("click", (event) =>
+        this.handleBoardClick(event),
+      );
+    }
+
+    this.startGame();
+  }
+
+  startGame() {
+    this.nextTurn();
+    this.moveIntervalId = setInterval(() => this.nextTurn(), MOVE_INTERVAL_MS);
+  }
+
+  nextTurn() {
+    if (this.currentHoleIndex !== -1 && !this.goblinClickedInThisTurn) {
+      this.losses++;
+      this.board.updateCounters(this.wins, this.losses);
+
+      if (this.losses >= MAX_LOSSES) {
+        this.endGame();
+        return;
+      }
+    }
+
+    this.goblinClickedInThisTurn = false;
+    this.goblin.remove();
+
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * this.board.holes.length);
+    } while (newIndex === this.currentHoleIndex && this.board.holes.length > 1);
+
+    this.board.holes[newIndex].append(this.goblin.element);
+    this.currentHoleIndex = newIndex;
+  }
+
+  handleBoardClick(event) {
+    const clickedHole = event.target.closest(".hole");
+
+    if (
+      clickedHole &&
+      clickedHole.contains(this.goblin.element) &&
+      !this.goblinClickedInThisTurn
+    ) {
+      this.goblinClickedInThisTurn = true;
+      this.wins++;
+      this.board.updateCounters(this.wins, this.losses);
+      this.goblin.remove();
+    }
+  }
+
+  endGame() {
+    this.stopInterval();
+    this.goblin.remove();
+    alert(`Игра окончена! Ваши очки: ${this.wins}`);
+  }
+
+  stopInterval() {
+    if (this.moveIntervalId !== null) {
+      clearInterval(this.moveIntervalId);
+      this.moveIntervalId = null;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const board = new Gameboard();
+  const goblin = new Goblin();
+  const game = new GameController(board, goblin);
+
+  game.init();
+
+  window.addEventListener("unload", () => game.stopInterval());
 });
